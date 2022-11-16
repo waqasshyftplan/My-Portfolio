7c5f1520a326bb88da05afa932174ab8543da943
waqasshyftplan <waqas@shyftplan.com>
2022-11-16 12:39:18 +0100

    crud operation complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController 
+  before_action:set_article, only: [:show, :edit, :update, :destroy]
   def show
-    @article=Article.find(params[:id])
+   
   end
 
   def index
@@ -11,7 +12,7 @@ class ArticlesController < ApplicationController
     @article=Article.new
   end
  def edit 
-  @article=Article.find(params[:id])
+  
  end 
  def create
     @article=Article.new(params.require(:article).permit(:title,:discription))
@@ -25,7 +26,7 @@ class ArticlesController < ApplicationController
   
  end
  def update
-  @article=Article.find(params[:id])
+ 
 
     if 
       @article.update(params.require(:article).permit(:title,:discription))
@@ -36,9 +37,13 @@ class ArticlesController < ApplicationController
     end 
  end 
  def destroy
-  @article=Article.find(params[:id])
+  
     @article.destroy
     redirect_to articles_path
+ end 
+ private
+ def set_article
+  @article=Article.find(params[:id])
  end 
 
 end 
