ffa7dd1d6449844661b603f703f8171ccc783b90
waqasshyftplan <waqas@shyftplan.com>
2022-11-23 14:20:10 +0100

    adding email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
-
+  validates:username,length:{minimum:3,maximum:25},presence:true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  validates:email,length:{minimum:12},presence:true,
+                  uniqueness:true,format:{with:VALID_EMAIL_REGEX}
 
 end 
